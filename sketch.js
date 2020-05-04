39fd708aaf72bfe02e02517fd56c68369c348c58
ananyaramkumar <ananyaramkumar06@gmail.com>
2020-05-04 15:37:42 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,49 +1,14 @@
-// Timer
-// https://youtu.be/MLtAMg9_Svw
-
-var timeleft = 10;
-
-var startTime = 0;
-var currentTime = 0;
-
-function convertSeconds(s) {
-  // WRITE CODE HERE
-}
-
-var ding;
-
-function preload() {
-  ding = loadSound('ding.mp3');
-}
-
-function setup() {
-  noCanvas();
-  startTime = millis();
-
-  var params = getURLParams();
-  console.log(params);
-  if (params.minute) {
-    var min = params.minute;
-    timeleft = min * 60;
-  }
-
-  var timer = select('#timer');
-  timer.html(convertSeconds(timeleft - currentTime));
-
-  var interval = setInterval(timeIt, 1000);
-
-  function timeIt() {
-    // WRITE CODE HERE
-  }
-}
+document.addEventListener('DOMContentLoaded', () => {
+	const timeLeftDisplay = document.querySelector('#time-left')
+	const startBtn = document.querySelector('#start-button')
+	//time variable here!
 
 
+	function countDown(){
+		//WRITE CODE for time variable here!
+	}
 
 
+	startBtn.addEventListener('click', countDown)
 
-
-
-
-
-
-// Credit to Daniel Shiffman
+})
